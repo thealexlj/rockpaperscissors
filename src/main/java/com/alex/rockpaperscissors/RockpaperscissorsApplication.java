3a4948b4793426a77f463e790ca552cea27d4aff
Alex <lj.alex@gmail.com>
2021-01-26 09:33:18 +0100

    initial commit, set up project

---
+++ src/main/java/com/alex/rockpaperscissors/RockpaperscissorsApplication.java
@@ -0,0 +1,13 @@
+package com.alex.rockpaperscissors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockpaperscissorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockpaperscissorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alex/rockpaperscissors/RockpaperscissorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.rockpaperscissors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockpaperscissorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
